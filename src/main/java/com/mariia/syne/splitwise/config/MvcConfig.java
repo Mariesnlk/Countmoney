709060658889d4c613590dadf105469e580e5c12
Mariesnlk <44849051+Mariesnlk@users.noreply.github.com>
2021-03-16 13:54:37 +0200

    added index and login page

--- src/main/java/com/mariia/syne/splitwise/config/MvcConfig.java
+++ src/main/java/com/mariia/syne/splitwise/config/MvcConfig.java
@@ -19,7 +19,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(
-                "/images/**",
+                "/static/images/**",
                 "/css/**",
                 "/js/**")
                 .addResourceLocations(
